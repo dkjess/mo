92b2a3976b62c57f6d64cc3d1c408614640d3c9c
Jess <dkjess@gmail.com>
2024-11-20 17:22:51 +0100

    Fixed / bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,8 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="Turbo Tube" />
-        <link rel="shortcut icon" href="/icons/icon-192x192.png">
-        <!-- <link rel="apple-touch-icon" href="/icons/icon-192x192.png"> -->
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png">
+        <link rel="shortcut icon" href="/icons/icon-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
         
         <link rel="manifest" href="/manifest.json" />
         
